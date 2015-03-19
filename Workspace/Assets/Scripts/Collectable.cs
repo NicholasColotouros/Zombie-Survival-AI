f13613f71b67c037b8eb591623cdc29309738985
Nicholas Nathan Colotouros <agentnnc@gmail.com>
2015-03-19 00:14:36 -0400

    fixed pickup trigger

--- Workspace/Assets/Scripts/Collectable.cs
+++ Workspace/Assets/Scripts/Collectable.cs
@@ -4,11 +4,8 @@ using System.Collections;
 //Destroy the sphere when the player enters
 public class Collectable : MonoBehaviour 
 {
-	void OnCollisionEnter(Collision collision)
+	void OnTriggerEnter(Collider collision)
 	{
-		if( collision.transform.tag == "Player")
-		{
-			Destroy(gameObject);
-		}
+		Destroy(gameObject);
 	}
 }
---
+++ Workspace/Assets/Scripts/SurvivorController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// used to test the game by allowing a human to play as the survivor
+public class SurvivorController : MonoBehaviour 
+{
+	public float MovementSpeed;
+	public float ZoomSpeed;
+	
+	// Controls
+	private KeyCode UP 		= KeyCode.UpArrow;
+	private KeyCode DOWN 	= KeyCode.DownArrow;
+	private KeyCode LEFT	= KeyCode.LeftArrow;
+	private KeyCode RIGHT 	= KeyCode.RightArrow;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		// up/down
+		if( Input.GetKey(UP) )
+		{
+			gameObject.transform.position += Vector3.forward * Time.deltaTime * MovementSpeed;
+		}
+		else if( Input.GetKey(DOWN) )
+		{
+			gameObject.transform.position += Vector3.back * Time.deltaTime * MovementSpeed;
+		}
+		
+		// left/right
+		if( Input.GetKey(LEFT))
+		{
+			gameObject.transform.position += Vector3.left * Time.deltaTime * MovementSpeed;
+		}
+		else if( Input.GetKey(RIGHT))
+		{
+			gameObject.transform.position += Vector3.right * Time.deltaTime * MovementSpeed;
+		}		
+	}
+}
