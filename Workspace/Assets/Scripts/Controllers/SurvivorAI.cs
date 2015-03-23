f494048979cdd2b1ee0d3f23d8d6540a0ae6d25d
Nicholas Nathan Colotouros <agentnnc@gmail.com>
2015-03-23 11:46:56 -0400

    improved level aesthetic to denote spawn points

---
+++ Workspace/Assets/Scripts/Controllers/SurvivorAI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SurvivorAI : MonoBehaviour 
+{
+	public Transform[] Collectables;
+	public Transform[] SafeSpots;
+	public Transform goal;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
